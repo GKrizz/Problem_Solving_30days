f568d03e8bb1fcba1339873c3ebd85ed6f22484f
GobalaKrishnan <103564745+GKrizz@users.noreply.github.com>
2024-08-15 19:14:37 +0530

    Day 1

--- program.c
+++ program.c
@@ -33,3 +33,46 @@ int main() {
     return 0;
 }
 
+//Sum of Even Numbers in an Array  
+#include <stdio.h>
+
+int main() {
+    int n, x, sum = 0;
+    scanf("%d", &n);  // Read the number of elements
+
+    // Directly process each number
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        sum += (x % 2 == 0) ? x : 0;  // Add to sum if the number is even
+    }
+
+    printf("%d\n", sum);  // Output the sum of even numbers
+
+    return 0;
+}
+
+//n-th Term of Arithmetic Progression
+#include <stdio.h>
+
+int main() {
+    int A, D, N;
+    scanf("%d %d %d", &A, &D, &N);
+    int nth_term = A + (N - 1) * D;
+    printf("%d\n", nth_term);
+    return 0;
+}
+
+//Can Ben Afford His Rent?
+#include <stdio.h>
+
+int main() {
+    int P, H, R;
+    scanf("%d %d %d", &P, &H, &R);
+    int total_income = P * H;
+
+    printf("%s\n", (total_income == R) ? "BARELY" : (total_income > R) ? "YES" : "NO");
+    
+    return 0;
+}
+
+
